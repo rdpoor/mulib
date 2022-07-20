5208d4496b833c05c975fe5442a0d1963a0bf7c5
R. D. Poor <rdpoor@gmail.com>
2022-07-20 12:51:54 -0700

    mu_time is now part of mulib core

---
+++ mulib/core/mu_time.c
@@ -0,0 +1,77 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2021 R. D. Poor <rdpoor@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// *****************************************************************************
+// Includes
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// *****************************************************************************
+// Private types and definitions
+
+// *****************************************************************************
+// Private (static) storage
+
+// *****************************************************************************
+// Private (forward) declarations
+
+// *****************************************************************************
+// Public code
+
+mu_time_abs_t mu_time_now(void) {
+  return mu_stdbsp_now();              // platform specific implementation
+}
+
+mu_time_abs_t mu_time_offset(mu_time_abs_t t, mu_time_rel_t dt) {
+  return t + dt;
+}
+
+mu_time_rel_t mu_time_difference(mu_time_abs_t t1, mu_time_abs_t t2) {
+  return t1 - t2;
+}
+
+bool mu_time_precedes(mu_time_abs_t t1, mu_time_abs_t t2) {
+  return (t1 - t2) > MU_TIME_REL_MAX;
+}
+
+bool mu_time_equals(mu_time_abs_t t1, mu_time_abs_t t2) { return t1 == t2; }
+
+bool mu_time_follows(mu_time_abs_t t1, mu_time_abs_t t2) {
+  return (t1 - t2) < MU_TIME_REL_MAX;
+}
+
+int mu_time_rel_to_ms(mu_time_rel_t dt) {
+  // TODO: reinstate integer rounding fn
+  return (dt * 1000) / MU_TIME_TICKS_PER_SECOND;
+}
+
+mu_time_rel_t mu_time_ms_to_rel(int ms) {
+  // TODO: reinstate integer rounding fn
+  return ms * MU_TIME_TICKS_PER_SECOND / 1000;
+}
+
+// *****************************************************************************
+// Private (static) code
